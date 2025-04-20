d178bb260c826ad4df3c0c63462b046f0d7e2842
Tuan Kiet <kiet.truong12082004@hcmut.edu.vn>
2025-04-20 20:44:48 +0700

    Add auto button

--- src/pages/Dashboard/Control/Control.js
+++ src/pages/Dashboard/Control/Control.js
@@ -8,6 +8,25 @@ function Control() {
             <div class="ControlHeader">
                 <h2>Control</h2>
             </div>
+            <div class="auto-button">  
+                <span>
+                        Auto
+                </span>
+                <div className="toggle-switch-auto active">
+                    
+                    <input 
+                        type="checkbox" 
+                        id="auto-toggle" 
+                        className="toggle-input-auto"
+                        // checked={isActive}
+                        // onChange={handleToggle}
+                        // disabled={isLoading}
+                    />
+                    <label htmlFor="auto-toggle" className="toggle-label-auto">
+                        <span className="toggle-button-auto"></span>
+                    </label>
+                </div>
+            </div>
 
             <div class="Grid">
                 <Light/>
