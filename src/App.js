78693edaf349108960f5ccb45d8dbc2cece81f8b
Khoa Tran <ovn6002@gmail.com>
2025-05-04 20:50:10 +0700

    refactor: move sidebar and header to src/components/

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 // import HomePage from './pages/HomePage/HomePage';
 import DashboardPage from './pages/Dashboard/DashboardPage';
 import Login from './pages/Login/Login';
+// import DeviceConfigPage from './pages/DeviceConfiguration/DeviceConfigPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/login" element={<Login />} />
+        {/* <Route path="/device-configuration" element={<DeviceConfigPage/>} /> */}
       </Routes>
     </BrowserRouter>
   )
